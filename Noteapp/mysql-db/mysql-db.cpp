e81f7506053ad8a43dac1d3f0002361c2fe656c3
Sabotaasi <h.oskari.heinonen@phnet.fi>
2015-04-02 10:21:55 +0300

    Added mysql-db .dll project

---
+++ Noteapp/mysql-db/mysql-db.cpp
@@ -0,0 +1,17 @@
+// mysql-db.cpp : Defines the exported functions for the DLL application.
+//
+
+/**
+
+__declspec(dllexport) function edess‰ tarkoittaa sit‰, ett‰ funktio menee
+eksportattavaan libbiin, jota executablen linkkauksessa k‰ytet‰‰n.
+
+esim.
+double __declspec(dllexport) sum(double x, double y);
+
+
+*/
+
+#include "stdafx.h"
+
+
