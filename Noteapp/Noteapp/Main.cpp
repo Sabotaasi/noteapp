de28b044af23b3da7b69700956346dcc7dad0684
migsmurai <mikko.pullinen@gmail.com>
2015-04-09 11:09:03 +0300

    rekisterin hallintaa

--- Noteapp/Noteapp/Main.cpp
+++ Noteapp/Noteapp/Main.cpp
@@ -1,66 +1,126 @@
 #include <iostream>
 #include "User.h"
+#include <Windows.h>
 
 using namespace std;
+
 void printHelp();
 #ifdef eng
 string helpmsg = "help\nmsg\neng";
 string failloginmsg = "Wrong username or password";
+string createfailmsg = "Username already exists";
+string createsuccessmsg = "Account was succesfully created";
+string registeryerrormsg = "Registry error";
 #else
 string helpmsg = "help\nmsg\nfi";
 string failloginmsg = "Vaara kayttajanimi tai salasana";
+string createfailmsg = "Kayttajanimi on jo olemassa";
+string createsuccessmsg = "Kayttajatilin luonti onnistui";
+string registeryerrormsg = "Rekisterivirhe";
 #endif
 
 int main(int argc, char *argv[]) 
 {
-	if (argc >= 6 && string(argv[1]) == "-u" && string(argv[3]) == "-p"){
-		User* user = new User(string(argv[2]), string(argv[3]));
-		if (user->isLoggedIn() == 0){
-			cout << failloginmsg << endl;
+	HKEY key;
+	LONG nError;
+	nError = RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\muistiosofta\\"), &key);
+	if (nError == ERROR_FILE_NOT_FOUND)
+	{
+		nError = RegCreateKeyEx(HKEY_CURRENT_USER, TEXT("Software\\muistiosofta\\"), NULL, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &key, NULL);
+		if (nError){
+			cout << registeryerrormsg << endl;
 		}
-		else{
-			//kirjota uname,pw,id to registery
-			if (argc == 6 && string(argv[5]) == "-nu"){
-				//create user
-				user->createMe();
-			}
-			else if (argc == 6 && string(argv[5]) == "-du"){
-				//delete user
-				user->deleteMe();
-			}
-			else if (argc == 7 && string(argv[5]) == "-w"){
-				//write new
-				user->writeNew(string(argv[6]));
-			}
-			else if (argc == 7 && string(argv[5]) == "-r"){
-				//read id
-				user->readMsg(atoi(argv[6]));
-			}
-			else if (argc == 7 && string(argv[5]) == "-d"){
-				//delete id
-				user->deleteMsg(atoi(argv[6]));
-			}
-			else if (argc == 6 && string(argv[5]) == "-l"){
-				//list all
-				user->listAllMsgs();
-			}
-			else{
-				printHelp();
-			}
-		}
-	}
-	else if (argc == 2 && string(argv[1]) == "-lo"){
-		//logout
 
 	}
-	else if (argc == 2 && string(argv[1]) == "-h"){
-		printHelp();
+	int offset = 0;
+	
+	//lue rekisteristä tunnarit useriin
+	string pw, uname;
+	char username[1024];		
+	DWORD u_size = sizeof(username);	
+	DWORD u_type = REG_SZ;
+	nError = RegQueryValueExA(key, "muistio_username", NULL, &u_type, (LPBYTE)&username, &u_size);
+	if (nError == ERROR_FILE_NOT_FOUND){
+		uname = '0';
 	}
 	else{
-		printHelp();
+		uname = string(username);
+	}
+	
+	
+	char password[1024];		
+	DWORD p_size = sizeof(password);	
+	DWORD p_type = REG_SZ;
+	nError = RegQueryValueExA(key, "muistio_password", NULL, &p_type, (LPBYTE)&password, &p_size);
+	if (nError == ERROR_FILE_NOT_FOUND){
+		pw = "0";
+	}
+	else{
+		pw = string(password);
 	}
 
+	User* user = new User(uname, pw);
+	if (argc > 1 && string(argv[1]) == "-u" && string(argv[3]) == "-p"){
+		//login
+		uname = string(argv[2]);
+		pw = string(argv[4]);
+		user = new User(uname, pw);
+		offset = 4;
+		if (argc > 1 + offset && string(argv[offset + 1]) == "-nu"){
+			//create user
+			if (user->createMe() != 0){
+				cout << createsuccessmsg << endl;
+			}
+			else{
+				cout << createfailmsg << endl;
+			}
+			return 0;
+		}
+	}
+	else if (argc > 1 && string(argv[1]) == "-lo"){
+		//logout
+		//pyyhi rekisteri
+		return 0;
+	}
+	else if (argc > 1 && string(argv[1]) == "-h"){
+		//hjap
+		printHelp();
+		return 0;
+	}
 
+	if (user->isLoggedIn() == 0){
+		//kirjoita rekisteriin tunnarit
+		RegSetValueExA(key, "muistio_password", 0, REG_SZ, (const BYTE*)pw.c_str(), pw.length() + 1);
+		RegSetValueExA(key, "muistio_username", 0, REG_SZ, (const BYTE*)uname.c_str(), uname.length() + 1);
+
+		if (argc > 1 + offset && string(argv[offset + 1]) == "-du"){
+			//delete user
+			user->deleteMe();
+		}
+		else if (argc > 1 + offset && string(argv[offset + 1]) == "-w"){
+			//write new
+			user->writeNew(string(argv[offset + 2]));
+		}
+		else if (argc > 1 + offset && string(argv[offset + 1]) == "-r"){
+			//read id
+			user->readMsg(atoi(argv[offset + 2]));
+		}
+		else if (argc > 1 + offset && string(argv[offset + 1]) == "-d"){
+			//delete id
+			user->deleteMsg(atoi(argv[offset + 2]));
+		}
+		else if (argc > 1 + offset && string(argv[offset + 1]) == "-l"){
+			//list all
+			user->listAllMsgs();
+		}
+		else{
+			printHelp();
+		}
+
+	}
+	else{
+		cout << failloginmsg << endl;
+	}
 
 	getchar();
 	return 0;
